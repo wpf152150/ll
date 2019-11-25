cf9f6e325452745cfaee34c45d88f51d27cbd91a
asus <asus@wpf>
2019-11-25 21:24:04 +0800

    33

--- src/entity/User.java
+++ src/entity/User.java
@@ -3,8 +3,7 @@ package entity;
 public class User {
 
 	public static void main(String[] args) {
-		//fryrfhyfhgh			//fryrfhyfhgh	//fryrfhyfhgh	//fryrfhyfhgh	//fryrfhyfhgh	//fryrfhyfhgh
-
+		//fryr
 	}
 
 }
